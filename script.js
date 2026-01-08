0c8a1639128793e4c5d03d6b88cf88b68ffe966c
Ayushi-1508 <Ayushi-1508@users.noreply.github.com>
2026-01-08 13:20:51 +0530

    Page1 done successfully.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
+function loadingAnimation(){
 var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
     duration:0.6,
     delay:0.5,
-})
+});
 tl.from("#line1-part1",{
     opacity:0,
     onStart: function(){
@@ -25,20 +25,40 @@ tl.from("#line1-part1",{
 tl.to('.line h2',{
     animationName: 'anime',
     opacity: 1,
-})
+});
 tl.to("#loader" , {
     opacity:0,
     duration:0.4,
     delay:3.2,
-})
+});
 tl.from("#page1",{
     delay: 0.2,
     y: 1600,
     opacity: 0,
-    duration: 0.5,
-    duration: 0.5,
+    duration: 0.6,
     ease: Power4
-})
+});
 tl.to("#loader",{
     display: "none"
-})
+});
+tl.from("#nav",{
+    opacity:0
+});
+tl.from("#hero1 h1 , #hero2 h1, #hero3 h2, #hero4 h1",{
+    y: 120,
+    stagger: 0.2
+});
+}
+function cursorAnimation(){
+document.addEventListener("mousemove", function(dets){
+     gsap.to("#crsr", {
+        left : dets.x,
+        top : dets.y
+       });
+});
+
+Shery.makeMagnet("#nav-part2 h4");
+}
+
+loadingAnimation();
+cursorAnimation();
